6352bff29feed1662f17c5fa612e654a011ad2ff
kielkow <matheuskiel@fiorifer.com.br>
2019-11-23 18:06:20 -0300

    Styled Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <></>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-vr-confortavel-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>New Nike Shoes</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADD ON CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-vr-confortavel-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>New Nike Shoes</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADD ON CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-vr-confortavel-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>New Nike Shoes</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADD ON CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-vr-confortavel-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>New Nike Shoes</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADD ON CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-vr-confortavel-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>New Nike Shoes</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADD ON CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-vr-confortavel-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>New Nike Shoes</strong>
+        <span>R$250,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADD ON CART</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
