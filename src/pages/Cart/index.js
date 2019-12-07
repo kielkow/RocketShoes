6d333010ab5b91bdca3aa1eec38f23a2cd1ca233
kielkow <matheuskiel@fiorifer.com.br>
2019-12-06 21:15:26 -0300

    Remove product

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -8,7 +8,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -47,7 +47,12 @@ function Cart({ cart }) {
               </td>
               <td>
                 <div>
-                  <button type="button">
+                  <button
+                    type="button"
+                    onClick={() =>
+                      dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                    }
+                  >
                     <MdDelete size={20} color="#7159c1" />
                   </button>
                 </div>
